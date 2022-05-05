ee2f3a44d4ecf5286794c63d00d8fac66391d719
danielfaria <danielcordeirofaria@gmail.com>
2022-05-05 17:34:09 -0300

    alterações
    alterações

--- criptografo.js
+++ criptografo.js
@@ -1,4 +1,13 @@
-var botaoCriptografar = document.querySelector("texto");
+var textoParaCriptografar = document.querySelector("#textoInserido");
 
-botaoCriptografar.addEventListener(click, function(){
-    
+var botaoCriptografar = document.querySelector(".botaoCriptografar");
+
+botaoCriptografar.addEventListener("click", function(){
+    var resultadoTextoParaCriptografar = textoParaCriptografar.replace("e","enter");
+    //var textoParaCriptografar = textoParaCriptografar.replace("i","imes");
+    //var textoParaCriptografar = textoParaCriptografar.replace("a","ai");
+    //var textoParaCriptografar = textoParaCriptografar.replace("o","obter");
+    //var textoParaCriptografar = textoParaCriptografar.replace("u","ufat");
+    figuraDoQuadro.remove();
+    console.log(resultadoTextoParaCriptografar.value);
+});
