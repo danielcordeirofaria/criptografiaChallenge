c3dd5302967b2d0446281318dd5d90c2730ebf20
danielfaria <danielcordeirofaria@gmail.com>
2022-05-06 02:36:16 -0300

    encriptação pronta

--- criptografo.js
+++ criptografo.js
@@ -1,16 +1,30 @@
-var textArea = document.querySelector("#textoInserido");
-
-var textoParaCriptografar = textArea.value
-
 var botaoCriptografar = document.querySelector(".botaoCriptografar");
 
 botaoCriptografar.addEventListener("click", function(){
-    var resultadoTextoParaCriptografar = textoParaCriptografar.replace("e","enter");
-    //var textoParaCriptografar = textoParaCriptografar.replace("i","imes");
-    //var textoParaCriptografar = textoParaCriptografar.replace("a","ai");
-    //var textoParaCriptografar = textoParaCriptografar.replace("o","obter");
-    //var textoParaCriptografar = textoParaCriptografar.replace("u","ufat");
-    var exclusaoFiguradoQuadro = document.querySelector(".itensParaExcluir")
+
+    var textArea = document.querySelector("#textoInserido");
+
+    var textoParaCriptografar = textArea.value;
+
+    var substituindoLetraE = textoParaCriptografar.replaceAll("e","enter");
+
+    var SubstituindoLetraI = substituindoLetraE.replaceAll("i","imes");
+    
+    var SubstituindoLetraA = SubstituindoLetraI.replace("a","ai");
+
+    var SubstituindoLetraO = SubstituindoLetraA.replace("o","obter");
+
+    var SubstituindoLetraU = SubstituindoLetraO.replace("u","ufat");
+
+    var exclusaoFiguradoQuadro = document.querySelector(".itensParaExcluir");
     exclusaoFiguradoQuadro.parentNode.removeChild(exclusaoFiguradoQuadro);
-    console.log(resultadoTextoParaCriptografar);
+    
 });
+
+
+
+//function textoParaCopiar{
+  //  var textoCriptografado = SubstituindoLetraU;
+    //textoCriptografado.appendChild()
+//}
+
